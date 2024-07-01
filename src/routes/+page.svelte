b2119f1e4558bd5f94ae9e9e1613f5b58ce631ca
Eric <45815665+distky@users.noreply.github.com>
2024-07-01 21:57:23 +0700

    feat: add phosphor icon example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+	import Cube from 'phosphor-svelte/lib/Cube';
+</script>
+
+<Cube color="darkorchid" weight="duotone">
+	<animate attributeName="opacity" values="0;1;0" dur="4s" repeatCount="indefinite"></animate>
+	<animateTransform
+		attributeName="transform"
+		attributeType="XML"
+		type="rotate"
+		dur="5s"
+		from="0 0 0"
+		to="360 0 0"
+		repeatCount="indefinite"
+	></animateTransform>
+</Cube>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
