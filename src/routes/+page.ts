404707ced5324dfe126193d6a8471c460f640d65
Eric <45815665+distky@users.noreply.github.com>
2024-07-04 02:23:03 +0700

    feat: add setHeaders based on fetch response

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,15 @@
 import type { PageLoad } from './$types';
 import { placeholderSchema, type Placeholder } from './+page.svelte';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const data: Placeholder = placeholderSchema.parse(
-		await fetch('https://pokeapi.co/api/v2/pokemon').then((response) => response.json())
-	);
+export const load: PageLoad = async ({ fetch, setHeaders }) => {
+	const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+
+	const data: Placeholder = placeholderSchema.parse(await response.json());
+
+	setHeaders({
+		age: response.headers.get('age') || '',
+		'cache-control': response.headers.get('cache-control') || ''
+	});
 
 	return {
 		fetchData: data
