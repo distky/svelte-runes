7fc9e4657ab806ebcf38ac678ea27e82f5ae5ccb
Eric <45815665+distky@users.noreply.github.com>
2024-07-05 07:46:38 +0700

    chore: remove payments route

--- src/routes/payments/data-table/data-table-checkbox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { Checkbox } from '$lib/components/ui/checkbox';
-	import type { Writable } from 'svelte/store';
-
-	let { checked }: { checked: Writable<boolean> } = $props();
-</script>
-
-<Checkbox bind:checked={$checked} />
