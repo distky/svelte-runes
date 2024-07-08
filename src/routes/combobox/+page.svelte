0a2454dd25a9841ff17bb6a2f7a4542d24b3afd7
Eric <45815665+distky@users.noreply.github.com>
2024-07-09 02:02:45 +0700

    fix: fully reset select and combobox after submit

--- src/routes/combobox/+page.svelte
+++ src/routes/combobox/+page.svelte
@@ -49,6 +49,8 @@
 			onUpdated: ({ form: f }) => {
 				if (f.valid) {
 					toast.success(`You submitted ${JSON.stringify(f.data, null, 2)}`);
+					$favoriteFruitProxy = '';
+					$defaultFruitProxy = '';
 				} else {
 					toast.error('Please fix the errors in the form.');
 				}
@@ -96,6 +98,7 @@
 	$effect(() => {
 		if (!openSelect && !$defaultFruitProxy) {
 			selectedDefaultFruit = undefined;
+			console.log('test');
 		}
 	});
 </script>
@@ -110,38 +113,39 @@
 			<Form.Field {form} name="favoriteFruit">
 				<Form.Control let:attrs>
 					<Form.Label>Favorite Fruit</Form.Label>
-					<Combobox.Root
-						bind:inputValue
-						bind:touchedInput
-						bind:selected={selectedFavoriteFruit}
-						onSelectedChange={(selected) =>
-							selected?.value && ($favoriteFruitProxy = selected.value)}
-					>
-						<Combobox.Input
-							{...attrs}
-							name={undefined}
-							class="w-[180px]"
-							placeholder="Search a fruit"
-							aria-label="Search a fruit"
-						/>
-						<Combobox.HiddenInput bind:value={$favoriteFruitProxy} name={attrs.name} />
-						<Combobox.Content>
-							<Combobox.Group>
-								<Combobox.Label>Fruits</Combobox.Label>
-								{#each filteredFruits as fruit (fruit.value)}
-									<Combobox.Item
-										value={fruit.value}
-										label={fruit.label}
-										disabled={fruit.disabled}
-									/>
-								{:else}
-									<span class="block px-5 py-2 text-sm text-muted-foreground">
-										No results found
-									</span>
-								{/each}
-							</Combobox.Group>
-						</Combobox.Content>
-					</Combobox.Root>
+					{#key selectedFavoriteFruit}
+						<Combobox.Root
+							bind:inputValue
+							bind:touchedInput
+							bind:selected={selectedFavoriteFruit}
+							onSelectedChange={(selected) => selected && ($favoriteFruitProxy = selected.value)}
+						>
+							<Combobox.Input
+								{...attrs}
+								name={undefined}
+								class="w-[180px]"
+								placeholder="Search a fruit"
+								aria-label="Search a fruit"
+							/>
+							<Combobox.HiddenInput bind:value={$favoriteFruitProxy} name={attrs.name} />
+							<Combobox.Content>
+								<Combobox.Group>
+									<Combobox.Label>Fruits</Combobox.Label>
+									{#each filteredFruits as fruit (fruit.value)}
+										<Combobox.Item
+											value={fruit.value}
+											label={fruit.label}
+											disabled={fruit.disabled}
+										/>
+									{:else}
+										<span class="block px-5 py-2 text-sm text-muted-foreground">
+											No results found
+										</span>
+									{/each}
+								</Combobox.Group>
+							</Combobox.Content>
+						</Combobox.Root>
+					{/key}
 				</Form.Control>
 				<Form.Description>Please select your favorite fruit.</Form.Description>
 				<Form.FieldErrors />
@@ -150,20 +154,22 @@
 			<Form.Field {form} name="defaultFruit">
 				<Form.Control let:attrs>
 					<Form.Label>Default Fruit</Form.Label>
-					<Select.Root
-						bind:open={openSelect}
-						bind:selected={selectedDefaultFruit}
-						onSelectedChange={(v) => v?.value && ($defaultFruitProxy = v.value)}
-					>
-						<Select.Trigger {...attrs} class="w-[180px]">
-							<Select.Value placeholder="Select a fruit" />
-						</Select.Trigger>
-						<Select.Content>
-							{#each fruitsWithoutNone as fruit}
-								<Select.Item value={fruit.value} label={fruit.label} disabled={fruit.disabled} />
-							{/each}
-						</Select.Content>
-					</Select.Root>
+					{#key selectedDefaultFruit}
+						<Select.Root
+							bind:open={openSelect}
+							bind:selected={selectedDefaultFruit}
+							onSelectedChange={(v) => v && ($defaultFruitProxy = v.value)}
+						>
+							<Select.Trigger {...attrs} class="w-[180px]">
+								<Select.Value placeholder="Select a fruit" />
+							</Select.Trigger>
+							<Select.Content>
+								{#each fruitsWithoutNone as fruit}
+									<Select.Item value={fruit.value} label={fruit.label} disabled={fruit.disabled} />
+								{/each}
+							</Select.Content>
+						</Select.Root>
+					{/key}
 					<input hidden bind:value={$defaultFruitProxy} name={attrs.name} />
 				</Form.Control>
 				<Form.Description>Please select your default fruit.</Form.Description>
