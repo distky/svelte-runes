c99f84b27351cdcde62763f06d2da22ec83fa6dd
Eric <45815665+distky@users.noreply.github.com>
2024-07-09 20:52:47 +0700

    fix(combobox-input): change pointer-events-auto to none

--- src/lib/components/ui/combobox/combobox-input.svelte
+++ src/lib/components/ui/combobox/combobox-input.svelte
@@ -11,7 +11,7 @@
 
 <div class={cn('relative', className)}>
 	<ChevronDown
-		class="pointer-events-auto absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 transform opacity-50"
+		class="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 transform opacity-50"
 	/>
 	<ComboboxPrimitive.Input
 		class="block h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 pr-8 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 aria-[invalid]:border-destructive [&>span]:line-clamp-1 data-[placeholder]:[&>span]:text-muted-foreground"
